113cf847683059eb601f5f2928c038af7ff33082
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2024-12-28 11:01:44 +0530

    add TailwindCSS setup with initial configuration files and index page

---
+++ week1/Day4/ProjectTailwind/tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+// filepath: /e:/WOC/week1/Day4/ProjectTailwind/tailwind/tailwind.config.js
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
