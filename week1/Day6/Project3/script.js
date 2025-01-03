794a145805d0d6424a8f15502fa4cd0e2e180a31
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-01-03 12:41:50 +0530

    enhance pop-up functionality with Font Awesome icons and improved styling

--- week1/Day6/Project3/script.js
+++ week1/Day6/Project3/script.js
@@ -1,7 +1,7 @@
 let popUp = () =>{
     let pop_box = document.createElement('div');
     pop_box.classList.add('pop');
-    pop_box.innerHTML = '<h1>Pop Up</h1><p>This is a pop up</p><button onclick="closePop()">Close</button>';
+    pop_box.innerHTML = '<h1>Pop Up</h1><p>This is a pop up</p><button class="close" onclick="closePop()"><i class="fa-sharp fa-solid fa-xmark"></i></button>';
     document.body.appendChild(pop_box);
     let notshow = document.querySelector('.btn');
     notshow.style.display = 'none';
