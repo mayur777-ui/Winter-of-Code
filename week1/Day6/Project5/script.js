6d44efe97f89883931834c98c43ba0b5bf01c22d
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-01-05 00:24:04 +0530

    add

--- week1/Day6/Project5/script.js
+++ week1/Day6/Project5/script.js
@@ -1,17 +1,17 @@
-// // let dragedItem = document.getElementById("drag1").innerText;
-// let drag = (e) => {
-//     e.dataTransfer.setData("text/plain", e.target.id);
-//     // console.log(`${dragedItem} is draged`);
-// }
+// let dragedItem = document.getElementById("drag1").innerText;
+let drag = (e) => {
+    e.dataTransfer.setData("text/plain", e.target.id);
+    // console.log(`${dragedItem} is draged`);
+}
 
-// let allowDrop = (e) => {
-//     e.preventDefault();
-// };
+let allowDrop = (e) => {
+    e.preventDefault();
+};
 
-// let drop = (e) => {
-//     e.preventDefault();
-//     let fetchData = e.dataTransfer.getData("text/plain");
-//     e.target.appendChild(document.getElementById(fetchData));
-//     // console.log(`${dragedItem} is dropped`);
-// }
+let drop = (e) => {
+    e.preventDefault();
+    let fetchData = e.dataTransfer.getData("text/plain");
+    e.target.appendChild(document.getElementById(fetchData));
+    // console.log(`${dragedItem} is dropped`);
+}
 
