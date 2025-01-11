2fad8e0c43004c74203f6a741e2667d45d0b1819
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-01-11 20:18:43 +0530

    feat: implement basic logging functionality and initial project structure

---
+++ week2/day3/app.js
@@ -0,0 +1,9 @@
+
+var url = "http://mylearning/log";
+
+function log(msg){
+    console.log(msg);
+}
+module.exports.log = log;
+module.exports.url = url;log
+
---
+++ week2/day3/index.js
@@ -0,0 +1,19 @@
+// const logger = require('./app.js');
+// logger.log('Hello World');
+
+// const os = require('os');
+
+
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+// var upTime = os.uptime()
+
+
+// console.log(totalMemory);
+// console.log(freeMemory);
+// console.log(upTime);
+// console.log(`Up Time is ${upTime}`);
+
+
+//file system module
+ 
